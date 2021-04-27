69ab0e15f8975ac12e0dc6d0ef1695be7eb8cae7
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-27 13:33:16 +0200

    refactor for easier readibility

--- controllers/api.js
+++ controllers/api.js
@@ -3,25 +3,21 @@ exports.getHello = (req, res, next) => {
 };
 
 exports.getDate = (req, res, next) => {
-  //try unix by default
-  let date = new Date(+req.params.date);
+  let date;
 
-  //if we have some params
+  console.log(date);
+
   if (req.params.date !== undefined) {
-    //if not valid unix timestamp
-    if (!date.getTime() > 0) {
-      //try date
-      date = new Date(req.params.date);
-    }
+    const unixTimestamp = +req.params.date;
+
+    date = isNaN(unixTimestamp)
+      ? new Date(req.params.date)
+      : new Date(unixTimestamp);
   } else {
-    //if we have not params then set actual date
     date = new Date(Date.now());
   }
 
-  // console.log(date);
-
   const response =
-    //if date is invalid
     date == 'Invalid Date'
       ? { error: 'Invalid Date' }
       : {
--- server.js
+++ server.js
@@ -22,5 +22,3 @@ app.use('/api', apiRoutes);
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-// app.listen(3010);
